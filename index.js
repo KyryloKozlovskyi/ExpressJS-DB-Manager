04260802d8baf2d4a5d7bbff71756b058f86fb9a
Kyrylo Kozlovskyi <g00425385@atu.ie>
2024-12-14 22:03:55 +0000

    Ejs view added for the index page.

--- index.js
+++ index.js
@@ -2,6 +2,10 @@
 var express = require("express");
 var app = express();
 
+// Set view engine
+let ejs = require("ejs");
+app.set("view engine", "ejs");
+
 // Port listener
 app.listen(3004, () => {
   console.log("Application listening on port 3004");
@@ -10,5 +14,5 @@ app.listen(3004, () => {
 // Home
 app.get("/", (req, res) => {
   console.log("GET");
-  res.send("<h1>This is a Home Page!</h1>");
+  res.render("index");
 });
